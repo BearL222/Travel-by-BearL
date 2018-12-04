fab88af0978e2f20097c6535f64e327a6486b4a9
BearL222 <429274165@qq.com>
2018-12-04 12:37:30 -0800

    swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
+    <div></div>
   </div>
 </template>
 
 <script>
 // 局部组件
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 
 export default {
-  name: 'Home',
-  components: {
-    HomeHeader
-  }
+	name: 'Home',
+	components: {
+	  HomeHeader,
+	  HomeSwiper
+	}
 }
 </script>
 
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='wrapper'>
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide><img src="https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?auto=compress&cs=tinysrgb&h=350"/></swiper-slide>
+        <swiper-slide><img src="https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&h=350"/></swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+    data () {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true
+      }
+  	}
+  }
+}
+
+</script>
+
+<style lang='stylus' scoped>
+  .wrapper
+    // 避免抖动
+    overflow: hidden
+    width: 100%
+    height: 0 
+    padding-bottom: 62.7%
+    background: #A9A9A9
+    .swiper-img
+      width: 100%
+</style>
